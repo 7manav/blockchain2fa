be240581d2a0c5237a6ae080e869360c7e63d563
7manav <149593379+7manav@users.noreply.github.com>
2025-04-24 17:11:17 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,10 +1,7 @@
 @echo off
 
-:: Open a new Command Prompt for the Hardhat deployment
 start cmd /k "cd C:\Users\Manav\blockchain && npx hardhat run scripts/deploy.js --network localhost"
 
-:: Open a new Command Prompt for the frontend
 start cmd /k "cd C:\Users\Manav\blockchain\frontend && npm run dev"
 
-:: Open a new Command Prompt for the backend
 start cmd /k "cd C:\Users\Manav\blockchain\backend && node server.js"
